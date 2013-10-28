691bba4d37e91bebebda7ff370e5c86ff075653f
aledalgrande <adgror@gmail.com>
2013-10-27 18:39:45 -0700

    refactoring of tests to comply with problem + count cut

--- solitaire_cipher.rb
+++ solitaire_cipher.rb
@@ -3,8 +3,74 @@ class SolitaireCipher
   def self.encrypt(message)
     filtered_string = message.upcase.gsub(/[^A-Z]/,'').strip
     padded_string_array = filtered_string.scan(/.....|....|...|..|./)
-    padded_string_array << padded_string_array.pop + 'X' * (5 - padded_string_array.size) if padded_string_array.last.size < 5
+    padded_string_array << padded_string_array.pop + 'X' * (5 - padded_string_array.size) if padded_string_array.last.size < 5  # pad the last one with Xs if needed
     padded_string_array.join(' ')
+  end
+
+  def self.deck
+    # ordered deck
+    base_deck
+
+    # move A joker down one card
+    move_card('A', 1)
+
+    # move B joker down two cards
+    move_card('B', 2)
+
+    # perform a triple cut around the jokers: cards on top of the first go to the bottom of the second and viceversa; cards in between them don't move
+    triple_cut
+
+    # perform a count cut using the value of the bottom card: cut the bottom card's value in cards off the top of the deck and reinsert them just above the bottom card
+    count_cut
+
+    # Find the output letter: convert the top card to its value and count down that many cards from the top of the deck, with the top card itself being card number one; look at the card immediately after your count and convert it to a letter; if the output card is a joker, no letter is generated this sequence; this step does not alter the deck
+    output_letter
+
+    p @deck
+  end
+
+  def self.base_deck
+    @deck = (1..52).to_a.map(&:to_s) + ['A', 'B']
+  end
+
+  def self.move_card(card, moves)
+    index = @deck.index(card)
+    size = @deck.size
+    @deck.delete_at(index)
+    new_index = (index + moves) % size
+    @deck.insert(new_index.eql?(0) ? 1 : new_index, card)  # array is smaller now
+  end
+
+  def self.triple_cut
+    top_deck, middle_deck, bottom_deck, passed_first_joker, passed_second_joker, first_joker, second_joker = [], [], [], false, false, nil, nil
+    @deck.each do |card, idx|
+      if card.eql?('A') || card.eql?('B')
+        if passed_first_joker
+          passed_second_joker = true
+          second_joker = card
+        else
+          passed_first_joker = true
+          first_joker = card
+        end
+      else
+        if passed_second_joker
+          bottom_deck << card
+        elsif passed_first_joker
+          middle_deck << card
+        else
+          top_deck << card
+        end
+      end
+      @deck = bottom_deck + [first_joker] + middle_deck + [second_joker] + top_deck
+    end
+    @deck
+  end
+
+  def self.count_cut
+    last = @deck.pop
+    num = last.to_i
+    @deck = @deck.slice(num, @deck.size - 1) + @deck.slice(0, num) + [last]
+    # 1, 2, 4, 5, 3 => (5, 3,) num:last (1, 2, 4) 0:num-1
   end
 
 end
--- spec/solitaire_ciper_spec.rb
+++ spec/solitaire_ciper_spec.rb
@@ -16,6 +16,54 @@ describe SolitaireCipher do
       SolitaireCipher.encrypt('I spent $400').should eq('ISPEN TXXXX')
     end
 
+    it "should generate a keystream letter for each letter" do
+      fail
+    end
+
+  end
+
+  context "keystream" do
+
+    before(:each) do
+      SolitaireCipher.base_deck      
+    end
+
+    it "should move a card" do
+      SolitaireCipher.move_card('A', 1)
+      SolitaireCipher.instance_variable_get(:@deck).should eq(["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "B", "A"])
+      SolitaireCipher.move_card('B', 2)
+      SolitaireCipher.instance_variable_get(:@deck).should eq(["1", "B", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "A"])
+    end
+
+    it "should perform triple cut" do
+      SolitaireCipher.move_card('A', 1)
+      SolitaireCipher.move_card('B', 2)
+      SolitaireCipher.triple_cut.should eq(["B", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "A", "1"])
+    end
+
+    it "should perform a count cut" do
+      SolitaireCipher.move_card('A', 1)
+      SolitaireCipher.move_card('B', 2)
+      SolitaireCipher.triple_cut
+      SolitaireCipher.count_cut.should eq(["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52", "A", "B", "1"])
+    end
+
+    it "should output a letter" do
+      fail
+    end
+
+    it "should not output anything if the output card is a joker" do
+      fail
+    end
+    
+    it "should create an unkeyed deck" do
+      SolitaireCipher.deck.first(10).should eq(['4', '49', '10', '53', '24', '8', '51', '44', '6', '4', '33'])
+    end
+
+    it "should generate a keystream" do
+      fail
+    end
+
   end
 
 end
